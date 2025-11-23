6200922074ba4f6e624bb5a2b21fba13b2d02a62
petro <peblynjhb024@student.wethinkcode.co.za>
2025-11-23 19:20:55 +0200

    move

--- snake.py
+++ snake.py
@@ -8,6 +8,11 @@ TILE_SIZE = 25
 WINDOW_WIDTH = TILE_SIZE * ROWS
 WINDOW_HEIGHT = TILE_SIZE * COLS
 
+class Tile:
+    def __init__(self, x , y):
+        self.x = x
+        self.y = y  
+
 #GAME WINDOW
 window = tkinter.Tk()
 window.title("Snake")
@@ -23,6 +28,55 @@ window_height = window.winfo_height()
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 
- 
+window_x = int((screen_width/2) - (window_width/2))
+window_y = int((screen_height/2) - (window_height/2))
+#format "(w)x(h)+(x)+(y)"
+window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}") 
 
+#initialize the game
+snake = Tile(5*TILE_SIZE, 5*TILE_SIZE)  #single tile, snake's head
+food = Tile(10*TILE_SIZE, 10*TILE_SIZE)  #single tile, food
+velocityX = 0
+velocityY = 0
+
+def change_direction(event):
+    global velocityX, velocityY
+
+
+    if (event.keysym == "Up"):
+        velocityX = 0
+        velocityY = -1
+    elif (event.keysym == "Down"):
+        velocityX = 0
+        velocityY = 1
+    elif (event.keysym == "Left"):
+        velocityX = -1
+        velocityY = 0
+    elif (event.keysym == "Right"):
+        velocityX = 1
+        velocityY = 0
+
+def move():
+    global snake
+
+    #update snake position
+    snake.x += velocityX * TILE_SIZE
+    snake.y += velocityY * TILE_SIZE
+
+
+def draw():
+    global snake
+    move()
+
+    #draw snake
+    canvas.create_rectangle(snake.x, snake.y, snake.x + TILE_SIZE, snake.y + TILE_SIZE, fill="lime green")
+
+    #draw food
+    canvas.create_rectangle(food.x, food.y, food.x + TILE_SIZE, food.y + TILE_SIZE, fill="red")
+
+    window.after(100, draw)  #redraw every 100 ms
+    
+draw()
+
+window.bind("<KeyRelease>", change_direction)
 window.mainloop()
